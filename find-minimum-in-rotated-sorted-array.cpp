d7e5e27a3c796a420057b8e3cddc2b33fcd51968
xuqiang <xq.1990.weihai@gmail.com>
2015-12-22 16:56:55 +0800

    find-minimum-in-rotated-sorted-array

---
+++ find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,25 @@
+/*
+
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	vector<int> numbers = { 3, 2, 4 };
+	Solution s;
+	
+	return 0;
+}
