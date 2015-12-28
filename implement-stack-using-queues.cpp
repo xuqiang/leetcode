c4943d2cc91d2828d5fa59bac47f0d4e0167240e
xuqiang <xq.1990.weihai@gmail.com>
2015-12-28 14:42:13 +0800

    implement stack using queue

--- implement-stack-using-queues.cpp
+++ implement-stack-using-queues.cpp
@@ -11,25 +11,41 @@ Implement Stack using Queues
 using namespace std;
 
 class Stack {
+private:
+    queue<int> q1, q2;
+
 public:
     // Push element x onto stack.
     void push(int x) {
-        
+        q2.push(x);
+        while (q2.size() > 1) {
+            q1.push(q2.front());
+            q2.pop();
+        }
     }
 
     // Removes the element on top of the stack.
     void pop() {
-        
+        top();
+        q2.pop();
     }
 
     // Get the top element.
     int top() {
-        
+        if (q2.empty()) {
+            for (int i = 0; i < (int)q1.size() - 1; ++i) {
+                q1.push(q1.front());
+                q1.pop();
+            }
+            q2.push(q1.front());
+            q1.pop();
+        }
+        return q2.front();
     }
 
     // Return whether the stack is empty.
     bool empty() {
-        
+        return q1.empty() && q2.empty();
     }
 };
 
