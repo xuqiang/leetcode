608d6a33f5449e65d834652787d213aaa117ca6c
xuqiang <xq.1990.weihai@gmail.com>
2015-12-29 19:05:55 +0800

    remove linked list elements

---
+++ remove-linked-list-elements.cpp
@@ -0,0 +1,43 @@
+/*
+
+Remove Linked List Elements
+
+
+Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5
+
+
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include "common.h"
+
+using namespace std;
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        
+    }
+};
+
+int main(int argc, char* argv[]) {
+  	return 0;
+}
