05dbd168f635615e5f2a23283c58b0cde468f120
xuqiang <xq.1990.weihai@gmail.com>
2015-12-29 17:19:28 +0800

    update count primes

--- count-primes.cpp
+++ count-primes.cpp
@@ -18,8 +18,7 @@ using namespace std;
 class Solution {
 public:
     int countPrimes(int n) {
-    	if(n <= 1) return 0;
-       if(n == 2) return 1;
+    	if(n <= 2) return 0;
        bool* p = new bool[n];
        memset(p, true, sizeof(bool) * n);
        for(int i = 2; i * i < n; ++i) {
