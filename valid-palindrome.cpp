4893c8e13ecc6aa5bdeec751225e720a28b4b583
xuqiang <xq.1990.weihai@gmail.com>
2015-12-30 11:20:10 +0800

    update

--- valid-palindrome.cpp
+++ valid-palindrome.cpp
@@ -2,7 +2,7 @@
 
 Valid Palindrome
 
-Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+Given a string, determine if it is a palindrome回文, considering only alphanumeric characters and ignoring cases.
 
 For example,
 "A man, a plan, a canal: Panama" is a palindrome.
