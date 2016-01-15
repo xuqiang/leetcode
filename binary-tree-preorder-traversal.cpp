980b8a50ad5861d7e199d561728d032936417893
xuqiang <xq.1990.weihai@gmail.com>
2016-01-15 11:51:34 +0800

    binary tree preorder traversal

---
+++ binary-tree-preorder-traversal.cpp
@@ -0,0 +1,51 @@
+/*
+
+Binary Tree Preorder Traversal
+
+二叉树前序遍历 非递归
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,2,3]
+
+*/
+
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include "common.h"
+
+using namespace std;
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+    }
+};
+
+
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
